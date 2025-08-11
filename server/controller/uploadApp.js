dddebd463afc25cbb76aeacba3b512f5829491fd
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2025-08-11 11:49:50 -0500

    delete uploads

--- server/controller/uploadApp.js
+++ server/controller/uploadApp.js
@@ -4,6 +4,7 @@ const ftp = require('basic-ftp')
 
 const SV_FILES = '172.27.74.54'
 const SV_FILES_PORT = 445
+const UPLOADS_DIR = path.join(__dirname, 'uploads')
 
 async function sendFile(zipPath, res) {
   try {
@@ -14,8 +15,8 @@ async function sendFile(zipPath, res) {
     await client.access({
       host: SV_FILES,
       port: SV_FILES_PORT,
-      user: 'guest',
-      password: '',
+      user: 'grupo2',
+      password: 'archivos',
       secure: false,
     })
 
@@ -24,17 +25,18 @@ async function sendFile(zipPath, res) {
 
     fs.unlinkSync(zipPath)
 
+    const fileToDelete = path.join(UPLOADS_DIR, fileName)
+    if (fs.existsSync(fileToDelete)) {
+      fs.unlinkSync(fileToDelete)
+    }
+
     if (!res.headersSent) {
-      res.send(
-        'Archivo zip enviado y procesado correctamente en el servidor remoto por FTP'
-      )
+      res.send('Archivo zip enviado y archivo en uploads eliminado')
     }
   } catch (error) {
     console.error('Error enviando zip por FTP:', error)
     if (!res.headersSent) {
-      res
-        .status(500)
-        .send('No se pudo enviar el archivo por FTP, intente más tarde')
+      res.status(500).send('Error al enviar o eliminar archivo')
     }
   }
 }
